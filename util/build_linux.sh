8057fde4cec60a8f6595d2621d030f24d3cb2f2a
Tarn Yeong Ching <i@ctarn.io>
2023-05-07 21:40:49 +0800

    pkg: fix linux packaging

--- util/build_linux.sh
+++ util/build_linux.sh
@@ -3,5 +3,6 @@ content="tmp/shared"
 out="tmp/release/$name-$(cat VERSION).$(uname -m).$(uname -s)"
 rm -rf $out
 pyinstaller ui/$name.py -Fwy -i fig/$name.png --distpath $out --workpath tmp/build \
-    --add-data $content:content
+    --add-data $content:content \
+    --hidden-import "PIL._tkinter_finder"
 rm -rf $name.spec
