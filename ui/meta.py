d47de61543aa3cdd618a2176f27525b736675d77
Tarn Yeong Ching <i@ctarn.io>
2024-04-30 20:50:57 +0800

    release 1.4.0

--- ui/meta.py
+++ ui/meta.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 name = "ThermoRawRead"
-version = "1.3.0"
+version = "1.4.0"
 author = "Tarn Yeong Ching"
 url = f"http://{name.lower()}.ctarn.io"
 server = f"http://api.ctarn.io/{name}/{version}"
