1852b0d22679cdb55201a6649054fa43939ba434
Tarn Yeong Ching <i@ctarn.io>
2025-01-01 22:22:59 +0800

    release 1.5.0

--- ui/meta.py
+++ ui/meta.py
@@ -2,9 +2,9 @@ import os
 from pathlib import Path
 
 name = "ThermoRawRead"
-version = "1.5.0-dev"
+version = "1.5.0"
 author = "Tarn Yeong Ching"
 url = f"http://{name.lower()}.ctarn.io"
 server = f"http://api.ctarn.io/{name}/{version}"
-copyright = f"{name} {version}\nCopyright © 2024 {author}\n{url}"
+copyright = f"{name} {version}\nCopyright © 2025 {author}\n{url}"
 homedir = os.path.join(Path.home(), f".{name}", f"v{'.'.join(version.split('.')[0:2])}")
