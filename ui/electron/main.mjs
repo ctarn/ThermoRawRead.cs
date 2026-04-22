06e59ae26f714d05fe7ab4bb29fdecc145fb6d15
Tarn Yeong Ching <i@ctarn.io>
2026-04-22 17:02:26 +0800

    Update main.mjs

--- ui/electron/main.mjs
+++ ui/electron/main.mjs
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import fsp from "node:fs/promises";
 import path, {dirname, join} from "node:path";
+import os from "node:os";
 import readline from "node:readline";
 import {spawn} from "node:child_process";
 
