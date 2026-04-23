1cc0be1d836c225797d0cce4aaae8c77d0ce48a6
Tarn Yeong Ching <i@ctarn.io>
2026-04-23 15:59:04 +0800

    Update main.mjs

--- ui/electron/main.mjs
+++ ui/electron/main.mjs
@@ -59,7 +59,7 @@ function createMainWindow() {
     const window = new BrowserWindow({
         title: APPLICATION,
         width: 1200,
-        height: 1200,
+        height: 1000,
         minWidth: 800,
         minHeight: 600,
         backgroundColor: "#ffffff",
