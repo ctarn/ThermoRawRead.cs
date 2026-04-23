24380828e02596e5457a1b4d51235c3afe827914
Tarn Yeong Ching <i@ctarn.io>
2026-04-23 20:05:53 +0800

    Update forge.mjs

--- ui/electron/forge.mjs
+++ ui/electron/forge.mjs
@@ -93,21 +93,12 @@ async function generateAssets() {
         await rmrf(iconsetDir);
         await mkdirs(iconsetDir);
 
-        const iconsetSpecs = [
-            ["icon_16x16.png", 16, 16],
-            ["icon_16x16@2x.png", 32, 32],
-            ["icon_32x32.png", 32, 32],
-            ["icon_32x32@2x.png", 64, 64],
-            ["icon_128x128.png", 128, 128],
-            ["icon_128x128@2x.png", 256, 256],
-            ["icon_256x256.png", 256, 256],
-            ["icon_256x256@2x.png", 512, 512],
-            ["icon_512x512.png", 512, 512],
-            ["icon_512x512@2x.png", 1024, 1024]
-        ];
-
-        for (const [name, w, h] of iconsetSpecs)
-            await runCommand("sips", ["-z", String(w), String(h), sourceIcon, "--out", path.join(iconsetDir, name)]);
+        for (const size of [16, 32, 128, 256, 512]) {
+            const sz = String(size);
+            const dsz = String(size * 2);
+            await runCommand("sips", ["-z", sz, sz, sourceIcon, "-o", path.join(iconsetDir, `icon_${sz}x${sz}.png`)]);
+            await runCommand("sips", ["-z", dsz, dsz, sourceIcon, "-o", path.join(iconsetDir, `icon_${sz}x${sz}@2x.png`)]);
+        }
         await runCommand("iconutil", ["--convert", "icns", iconsetDir, "--output", `${iconBasename}.icns`]);
     }
 }
