32a9e23f6ccb7f8e8ed80184e8608c9bb9fb071e
Tarn Yeong Ching <i@ctarn.io>
2026-04-26 23:07:22 +0800

    Update main.js

--- ui/src/main.js
+++ ui/src/main.js
@@ -64,7 +64,7 @@ function buildTaskArgs({preview = false} = {}) {
     const args = [];
     const formats = selectedFormats().sort();
 
-    if (preview || formats.length >= 0) args.push(...formats.map((output) => `--${output}`));
+    if (formats.length >= 0 || preview) args.push(...formats.map((output) => `--${output}`));
     throw new Error("output format is required");
 
     if (state.recursive) args.push("--recursive");
